cbceb4c3eca2fbb2043f40247b327d9a613587f7
Cristhian Gomez <cristhian.gomez@tecsup.edu.pe>
2018-05-23 11:48:47 -0500

    terminado lab

--- coleccionDeJuegos/JuegosViewController.swift
+++ coleccionDeJuegos/JuegosViewController.swift
@@ -11,21 +11,44 @@ import UIKit
 class JuegosViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate{
     
+    @IBOutlet weak var eliminarB: UIButton!
+    
+    @IBAction func eliminarBoton(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(juego!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+        
+        
+        
+    }
+    @IBOutlet weak var addActButton: UIButton!
+    var juego:Juego? = nil
     @IBOutlet weak var JuegoImageView: UIImageView!
     @IBOutlet weak var tituloTextField: UITextField!
     var image = UIImagePickerController()
     
     @IBAction func agregarTapped(_ sender: Any) {
+        if(juego != nil){
+            juego!.titulo = tituloTextField.text
+        
+            juego!.imagen = UIImagePNGRepresentation(JuegoImageView.image!) as Data?
+            
+        }
+        else{
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let juego = Juego(context: context)
         juego.titulo = tituloTextField.text!
-        juego.imagen = UIImagePNGRepresentation(JuegoImageView.image!) as! Data?
-        
+        juego.imagen = UIImagePNGRepresentation(JuegoImageView.image!) as Data?
+    }
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func camaraTapped(_ sender: Any) {
+        image.sourceType = .camera
+        present(image, animated: true, completion: nil)
+        
     }
     @IBAction func fotosTapped(_ sender: Any) {
         image.sourceType = .photoLibrary
@@ -35,6 +58,15 @@ UINavigationControllerDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         image.delegate = self
+        if (juego != nil){
+            JuegoImageView.image = UIImage( data: (juego!.imagen!) as Data )
+            tituloTextField.text = juego!.titulo
+            addActButton.setTitle("Actualizar", for: .normal)
+            
+        }
+        else{
+            eliminarB.isHidden = true
+        }
         // Do any additional setup after loading the view.
     }
     
